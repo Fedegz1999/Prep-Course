60e2e5e064ba099b5edc55df5a34fa0d45c93398
Fedegz1999 <federicoguti@gmail.com>
2022-04-11 16:53:39 -0300

    third homework

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -39,7 +39,8 @@ function agregarItemAlFinalDelArray(array, elemento) {
   // Añade el "elemento" al final del array
   // y devuelve el array
   // Tu código:
-  return array.push(elemento);
+array.push(elemento);
+return array;
 }
 
 
@@ -48,7 +49,8 @@ function agregarItemAlComienzoDelArray(array, elemento) {
   // y devuelve el array
   // Pista: usa el método `.unshift`
   // Tu código:
-  return array.unshift(elemento);
+array.unshift(elemento);
+return array;
 }
 
 
@@ -118,9 +120,18 @@ function multiplicarArgumentos() {
   // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
   // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
   // Escribe tu código aquí:
-  
+  var acumulador = 1
+  if (arguments.length === 0) {
+    return 0;
+  }else if (arguments.length === 1) {
+    return arguments[0]
+  }else {
+    for (var i = 0; i < arguments.length; i++) {
+      acumulador = acumulador * arguments[i]
+    }
+  }
+  return acumulador;
 }
-
 
 function cuentoElementos(arreglo){
   //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 18.
@@ -131,7 +142,7 @@ function cuentoElementos(arreglo){
       arrayCuenta.push(arreglo[i])
     }
   }
-   return arrayCuenta; 
+   return arrayCuenta.length; 
 }
 
 
@@ -169,10 +180,10 @@ function todosIguales(arreglo) {
   //Escribe tu código aquí  
   for (var i = 0; i < arreglo.length -1; i++) {
     if (arreglo[i] === arreglo[i+1]) {
-      return false;
+      return true;
     }
   }
-  return true;
+  return false;
 } 
 
 
@@ -200,7 +211,7 @@ function mayorACien(array) {
   //La función recibe un array con enteros entre 0 y 200. Recorrer el array y guardar en un nuevo array sólo los
   //valores mayores a 100 (no incluye el 100). Finalmente devolver el nuevo array.
   // Tu código:
-  var nuevoArray = 0
+  var nuevoArray = []
   for (let i = 0; i < array.length; i++) {
     if (array[i] > 100) {
       nuevoArray.push(array[i]);
@@ -219,6 +230,21 @@ function breakStatement(numero) {
   //devolver: "Se interrumpió la ejecución"
   //Pista: usá el statement 'break'
   // Tu código:
+  var arrayBucle = [];
+  var suma = numero 
+  for (var i = 0; i < 10; i++) {
+    suma = suma + 2;
+    if (suma === i) {
+      break;
+    } else{
+      arrayBucle.push(suma);
+    }
+  }
+  if (arrayBucle.length < 10) {
+    return "Se interrumpió la ejecución";
+  }else if (arrayBucle.length === 10){
+    return arrayBucle; 
+  }
 }
 
 
@@ -229,6 +255,17 @@ function continueStatement(numero) {
   //Cuando el número de iteraciones alcance el valor 5, no se suma en ese caso y se continua con la siguiente iteración
   //Pista: usá el statement 'continue'
   // Tu código:
+  var arrayBucle = [];
+  var suma = numero 
+  for (var i = 0; i < 10; i++) {
+    if (i === 5) {
+      continue;
+    } else{
+      suma = suma + 2;
+      arrayBucle.push(suma)
+    }
+  }
+  return arrayBucle;
 }
 
 
